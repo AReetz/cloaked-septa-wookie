dadc27d3555053c2ccf9e99889a9474ad0f174c0
Adrian <Adrian@10.81.7.208>
2014-02-02 23:29:52 -0600

    Detached head fix 4

--- src/net/hlw5a/VidPicLib/Ui/VPLStarter.java
+++ src/net/hlw5a/VidPicLib/Ui/VPLStarter.java
@@ -4,7 +4,6 @@ import javax.swing.SwingUtilities;
 
 public class VPLStarter {
 	public static void main(String[] args) {
-		int i = 0;
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "VidPicLib");
 		SwingUtilities.invokeLater(new VPLMainProgram());
